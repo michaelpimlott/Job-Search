67bae13a9aaa928d0d5c113689b1f9c4b2ca4a60
Michael Pimlott <pettingbutterscotch@gmail.com>
2016-06-29 15:23:08 -0700

    add controllers to routes

--- src/client/controllers.js
+++ src/client/controllers.js
@@ -5,3 +5,23 @@ myApp.controller('MainController', [
     $scope.test = 'Go Fuck Yourself.';
     $scope.test2be = 'go fek hmpge';
   }]);
+myApp.controller('LoginController', [
+  '$scope',
+  function($scope){
+    $scope.test = 'do whatever you want.';
+  }]);
+  myApp.controller('EditController', [
+    '$scope',
+    function($scope){
+      $scope.test = 'edit whatever you want.';
+    }]);
+    myApp.controller('NewController', [
+      '$scope',
+      function($scope){
+        $scope.test = 'New whatever you want.';
+      }]);
+      myApp.controller('ShowController', [
+        '$scope',
+        function($scope){
+          $scope.test = 'Show whatever you want.';
+        }]);
--- src/client/main.js
+++ src/client/main.js
@@ -6,16 +6,20 @@ myApp.config(['$routeProvider', function ($routeProvider) {
       templateUrl: 'partials/home.html'
     })
     .when('/login', {
-      templateUrl: 'partials/login.html'
+      templateUrl: 'partials/login.html',
+      controller: 'LoginController'
     })
     .when('/new', {
-      templateUrl: 'partials/newJob.html'
+      templateUrl: 'partials/newJob.html',
+      controller: 'NewController'
     })
     .when('/show', {
-      templateUrl: 'partials/showJob.html'
+      templateUrl: 'partials/showJob.html',
+      controller: 'ShowController'
     })
     .when('/edit', {
-      templateUrl: 'partials/edit.html'
+      templateUrl: 'partials/edit.html',
+      controller: 'EditController'
   })
     .otherwise({redirectTo: '/'});
 }]);
